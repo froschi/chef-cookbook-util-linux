055d3a17bdf90d76e48aff9b0ef6192ca8ccae9c
Thorsten Fischer <thorsten@froschi.org>
2012-06-20 14:25:00 +0200

    Added dependencies.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,11 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libblkid", ">= 0.0.1"
+depends          "libncurses", ">= 0.0.1"
+depends          "libselinux", ">= 0.0.1"
+depends          "libslang", ">= 0.0.1"
+depends          "libtinfo", ">= 0.0.1"
+depends          "libuuid", ">= 0.0.1"
+depends          "tzdata", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,20 @@
+include_recipe "libblkid"
+include_recipe "libncurses"
+include_recipe "libselinux"
+include_recipe "libslang"
+include_recipe "libuuid"
+include_recipe "tzdata"
+
 packages = Array.new
 
 case node[:lsb][:codename]
-when "lucid", "precise"
+when "lucid"
+  packages |= %w/
+    util-linux
+  /
+when "precise"
+  include_recipe "libtinfo"
+
   packages |= %w/
     util-linux
   /
